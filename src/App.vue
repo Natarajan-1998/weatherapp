478113022f403dd9e4ec1ffc158807b9f19aebc7
A V Natarajan <avn1998@gmail.com>
2021-05-29 16:43:42 +0530

    Added date builder

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,32 @@ export default {
   data() {
     return{
       api_key:"2685db14222dfebeb28333f520d1e4e7",
-      url_base: 'https://api.openweathermap.org/data/2.5/'
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
     }
   }
   }
